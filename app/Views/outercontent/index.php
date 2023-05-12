fcb2cc7fdf824a665fccfedec22484578d632bbd
IbrhmSptra <sptraibrhm@gmail.com>
2023-05-12 18:59:41 +0700

    Finishing AAGym. (Add Logo, icon web dan rapihin sedikit css)

--- app/Views/outercontent/index.php
+++ app/Views/outercontent/index.php
@@ -4,7 +4,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
-        <a class="navbar-brand" href="/">AAGym</a>
+        <a id="logonavbar" class="navbar-brand" href="/">AAGYM</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
